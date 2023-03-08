284930a9359f18683c8308c99f74b3139d50250c
stmkpfen <1Nightmail@web.de>
2023-03-08 23:54:29 +0100

    init commit

---
+++ SavegameHandlerClass.py
@@ -0,0 +1,91 @@
+import shutil
+import os
+
+class SavegameHandler:
+    config_file = 'SavegameManagerConfig.txt'
+
+    def __init__(self):
+        self.src_path = None
+        self.dest_path = None
+        self.last_saves = []
+    
+    def set_config(self, _src_path, _dest_path):
+        self.src_path = _src_path
+        self.dest_path = _dest_path
+
+    def load_config(self):
+        if os.path.exists(SavegameHandler.config_file):
+            with open(SavegameHandler.config_file, 'r') as f:
+                    self.src_path, self.dest_path = f.read().strip().split('\n')
+
+    def save_config(self):
+        with open(SavegameHandler.config_file, "w") as f:
+            f.write("\n".join([self.src_path, self.dest_path]))
+
+    def has_sl2_file(self, folder_path):
+        for dirpath, dirnames, filenames in os.walk(folder_path):
+            for filename in filenames:
+                if filename.lower().endswith('.sl2'):
+                    return True
+        return False
+
+    def config_valid(self):
+        if self.src_path is None:
+            return False, "Source path is not set"
+
+        if self.dest_path is None:
+            return False, "Destination path is not set"
+
+        if not os.path.exists(self.src_path):
+            return False, "Source path does not exist"
+
+        if not os.path.exists(self.dest_path):
+            return False, "Destination path does not exist"
+
+        if not self.has_sl2_file(self.src_path):
+            return False, "No SL2 file found in source path"
+
+        return True, None
+
+    def find_highest_index(self):
+        highest_index = -1
+        for subfolder_name in os.listdir(self.dest_path):
+            if subfolder_name.startswith("savegame_"):
+                index_str = subfolder_name[len("savegame_"):]
+                try:
+                    index = int(index_str)
+                    if index > highest_index:
+                        highest_index = index
+                except ValueError:
+                    # If the numeric part of the folder name isn't a valid integer, skip it
+                    pass
+        return highest_index
+
+    def save_data(self):
+        if not os.path.exists(self.src_path) or not os.path.exists(self.dest_path):
+            raise Exception("Both source and destination paths must exist.")
+    
+        src_size = sum(os.path.getsize(os.path.join(dirpath, filename)) for dirpath, dirnames, filenames in os.walk(self.src_path) for filename in filenames)
+        if src_size > 100 * 1024 * 1024:
+            raise Exception("Source directory is too large, it should be less than 100MB.")
+    
+        index = self.find_highest_index() + 1
+        savegame_folder_name = f"savegame_{index}"
+        save_folder = os.path.join(self.dest_path, savegame_folder_name)
+        shutil.copytree(self.src_path, save_folder)
+    
+    def load_data(self):
+        current = 0
+        while True:
+            index = self.find_highest_index() - current
+            current += 1
+            if index < 0:
+                return
+            savegame_folder_name = f"savegame_{index}"
+            save_folder = os.path.join(self.dest_path, savegame_folder_name)
+            if  os.path.exists(save_folder):
+                if self.has_sl2_file(save_folder):
+                    break
+        if os.path.exists(self.src_path):
+            shutil.rmtree(self.src_path)
+            shutil.copytree(save_folder, self.src_path)
---
+++ SavegameManager.py
@@ -0,0 +1,267 @@
+import PySimpleGUI as sg
+import keyboard
+import vlc
+from SavegameHandlerClass import SavegameHandler
+
+#hexvalues
+color_background_light = '#FFFFFF'
+color_boxes_light = '#969696'
+color_button_light = '#E1E1E1'
+color_text_light = '#000000'
+
+color_background_dark = '#222222'
+color_boxes_dark = '#696969'
+color_button_dark = '#1E3973'
+color_text_dark = '#FFFFFF'
+
+color_background = color_background_dark
+color_boxes = color_boxes_dark
+color_button = color_button_dark
+color_text = color_text_dark
+
+icon_path = 'res//images//SavegameManager.ico'
+textFont = ("Arial", 11)
+
+#audio_save = vlc.Media("res//audio//save.wav")
+#audio_load = vlc.Media("res//audio//load.wav")
+
+settings_volume = int(100)
+
+Instance = vlc.Instance()
+
+audio_save = Instance.media_player_new()
+audio_load = Instance.media_player_new()
+
+media_save = Instance.media_new("res//audio//save.wav")
+media_load = Instance.media_new("res//audio//load.wav")
+
+media_save.get_mrl()
+media_load.get_mrl()
+
+audio_save.set_media(media_save)
+audio_load.set_media(media_load)
+
+def play_audio(player):
+    global settings_volume
+    player.audio_set_volume(settings_volume)
+    player.stop()
+    player.play()
+    while player.get_state() != 6:
+        continue
+
+def on_press_f9(event):
+    if not isRunning:
+        return
+    if event.name == 'f9':
+        global handler
+        handler.load_data()
+        if sound:
+            play_audio(audio_load)
+
+def on_press_f5(event):
+    if not isRunning:
+        return
+    if event.name == 'f5':
+        global handler
+        handler.save_data()
+        if sound:
+            play_audio(audio_save)
+
+# Define your function that will be called when the button is pressed
+def selectsavegame_callback():
+    # Open the folder browser dialog and get the selected folder path
+    folder_path = sg.popup_get_folder("Select a savegame folder", default_path=window['-OUTPUT-'].get(), size=(80, 1), font=textFont, button_color=(color_text, color_button), background_color=color_boxes, icon=icon_path)
+
+    # Update the text of the output element to display the selected folder path
+    if folder_path:
+        window["-OUTPUT-"].update(f"{folder_path}")
+
+def selectbackup_callback():
+    # Open the folder browser dialog and get the selected folder path
+    folder_path = sg.popup_get_folder("Select a backup folder", default_path=window['-OUTPUT-2'].get(), size=(80, 1), font=textFont, button_color=(color_text, color_button), background_color=color_boxes, icon=icon_path)
+
+    # Update the text of the output element to display the selected folder path
+    if folder_path:
+        window["-OUTPUT-2"].update(f"{folder_path}")
+
+def dark_mode_toggle(toggle_value):
+    global color_background
+    global color_boxes
+    global color_button
+    global color_text
+
+    if toggle_value:
+        color_background = color_background_dark
+        color_boxes = color_boxes_dark
+        color_button = color_button_dark
+        color_text = color_text_dark
+    else:
+        color_background = color_background_light
+        color_boxes = color_boxes_light
+        color_button = color_button_light
+        color_text = color_text_light
+    
+    for element in window.element_list():
+        if isinstance(element, sg.Button):
+            element.update(button_color=(color_text, color_button))
+        if isinstance(element, sg.Text):
+            element.update(text_color=color_text)
+            element.update(background_color=color_background)
+        if isinstance(element, sg.Checkbox):
+            element.update(background_color=color_button)
+            element.update(text_color=color_text)
+            element.update(checkbox_color=color_boxes)
+        #if isinstance(element, sg.Frame):
+            #element.TKroot.configure(background=color0)
+
+    window.TKroot.configure(background=color_background)
+    window.Refresh()
+    
+
+    
+    #window["-TOGGLE-"].update(True)
+    #popup_window("there is no white mode!")
+
+# Define the layout of your GUI window
+layout = [
+    [sg.Frame("Savegame Location", [
+        [sg.Button("Select folder", key="-BUTTON-", size=(10,1), font=textFont),
+         sg.Text("no path", size=(80,1), text_color=color_text,  key="-OUTPUT-", background_color=color_background, font=textFont)]
+    ], border_width=1, relief="flat", background_color=color_boxes, key="-FRAME-")],
+    [sg.Frame("Backup Location", [
+        [sg.Button("Select folder", key="-BUTTON-2", size=(10,1), font=textFont),
+         sg.Text("no path", size=(80,1), text_color=color_text, key="-OUTPUT-2", background_color=color_background, font=textFont)]
+    ], border_width=1, relief="flat", background_color=color_boxes)],
+
+    [sg.Frame("", [
+        [sg.Button("Start", key="-BUTTON-START", size=(10,1), font=textFont),
+         sg.Button("Stop", key="-BUTTON-STOP", size=(10,1), disabled=True, font=textFont),
+         sg.Checkbox("Dark Mode", key="-TOGGLE-", size=(10,1), default=True, checkbox_color=color_boxes, background_color=color_button, enable_events=True, font=textFont, metadata=True),
+         sg.Button("Settings", key="-BUTTON_SETTINGS-", size=(10,1), disabled=False, font=textFont)]
+         
+    ], border_width=1, relief="flat", background_color=color_boxes)]
+
+]
+
+def custom_print(*args, **kwargs):
+    # Convert arguments to a string
+    message = " ".join(map(str, args))
+    
+    # Append the message to the output element
+    window['-OUTPUT_CONSOLE-'].print(message, **kwargs)
+
+isRunning=False
+
+def popup_window(message):
+    column_to_be_centered = [
+                             [sg.Text(message, text_color=color_text, background_color=color_boxes, font=textFont)],
+                             [sg.Button('Ok!', size=(150, 75), font=textFont)],
+                            ]
+
+    layoutConfigValid = [
+                         [sg.VPush(background_color=color_boxes)],
+                         [sg.Push(background_color=color_boxes), sg.Column(column_to_be_centered, element_justification='c', background_color=color_boxes), sg.Push()],
+                         [sg.VPush(background_color=color_boxes)]
+                        ]
+    windowConfigValid = sg.Window(message, layoutConfigValid, background_color=color_boxes, button_color=(color_text, color_button), size=(350,100), finalize=True, keep_on_top=True, modal=True)
+    windowConfigValid.set_icon(icon_path)
+    event, values = windowConfigValid.read(close=True)
+
+sound = True
+    
+
+# Create the window using the layout
+window = sg.Window("Savegame Manager", layout, background_color=color_background, finalize=True, button_color=(color_text, color_button))
+window.set_icon(icon_path)
+window.set_min_size((875, 175))
+
+handler = SavegameHandler()
+handler.load_config()
+
+def open_settings_menu():
+
+    global sound
+    global settings_volume
+    print("settings opened: " + str(sound))
+
+    layout_settings =   [
+                            [sg.Checkbox("Sound", key="-SOUND-", size=(10,1), default=sound, checkbox_color=color_boxes, background_color=color_button, enable_events=True, font=textFont, metadata=True)],
+                            [sg.Slider(range=(0, 100), orientation='horizontal', size=(20, 15), default_value=50, resolution=1, enable_events=True, key='-SLIDER-')],
+                            [sg.Button('Submit'), sg.Button('Cancel')]
+                        ]
+                        
+    window_settings = sg.Window("Settings", layout_settings, background_color=color_boxes, button_color=(color_text, color_button), size=(350,100), finalize=True, keep_on_top=True, modal=True)
+    window_settings.set_icon(icon_path)
+
+    while True:
+
+        event, values = window_settings.read()
+
+        if event == "-SOUND-":
+             sound = values["-SOUND-"]
+             print("sound checkbox pressed: " + str(sound))
+        if event == '-SLIDER-':
+            settings_volume = values["-SLIDER-"]
+
+        if event == sg.WINDOW_CLOSED or event == "Exit":
+            break
+
+    print("settings closed: " + str(sound))
+    window_settings.close()
+
+if not handler.config_valid():
+    popup_window('invalid paths')
+else:
+    window["-OUTPUT-"].update(handler.src_path)
+    window["-OUTPUT-2"].update(handler.dest_path)
+
+
+keyboard.on_release(on_press_f5)
+keyboard.on_release(on_press_f9)
+# Start the event loop to process events and wait for user interaction
+while True:
+
+    event, values = window.read()
+
+    # If the user closes the window or clicks the Exit button, exit the event loop
+    if event == sg.WINDOW_CLOSED or event == "Exit":
+        break
+
+    if event == "-TOGGLE-":
+        dark_mode_toggle(values['-TOGGLE-'])
+    # If the user clicks the button, call the button_callback function
+    if event == "-BUTTON-":
+        selectsavegame_callback()
+
+    if event == "-BUTTON-2":
+        selectbackup_callback()
+
+    if event == "-BUTTON_SETTINGS-":
+        open_settings_menu()
+
+    if event == "-BUTTON-START":
+        handler.set_config(window['-OUTPUT-'].get(), window['-OUTPUT-2'].get())
+        is_valid, error_msg = handler.config_valid()
+        if is_valid:
+            window["-BUTTON-"].update(disabled=True)
+            window["-BUTTON-2"].update(disabled=True)
+            window["-BUTTON-STOP"].update(disabled=False)
+            window["-BUTTON-START"].update(disabled=True)
+            isRunning = True
+        else:
+            popup_window(error_msg)
+    
+    if event == "-BUTTON-STOP":
+        window["-BUTTON-"].update(disabled=False)
+        window["-BUTTON-2"].update(disabled=False)
+        window["-BUTTON-START"].update(disabled=False)
+        window["-BUTTON-STOP"].update(disabled=True)
+        isRunning=False
+
+
+# Close the window and exit the program
+is_valid, error_msg = handler.config_valid()
+if is_valid:
+    handler.save_config()
+    
+window.close()
